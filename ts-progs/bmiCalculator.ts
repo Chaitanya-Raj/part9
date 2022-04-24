881050d52a057fe880658dc39ffdef81154e113a
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-04-24 14:11:54 +0530

    9.5

--- ts-progs/bmiCalculator.ts
+++ ts-progs/bmiCalculator.ts
@@ -17,7 +17,7 @@ const parseArguments = (args: Array<string>): BmiValues => {
   }
 };
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi: number = (weight / height / height) * 10000;
   if (bmi < 18.5) {
     return "Underweight";
--- ts-progs/index.ts
+++ ts-progs/index.ts
@@ -1,12 +1,28 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
 
-const PORT = 3003;
+app.get("/bmi", (req, res) => {
+  try {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+    if (!height || !weight) {
+      throw "malformatted parameters";
+    }
+    const bmi = calculateBmi(height, weight);
+    res.send({ weight, height, bmi });
+  } catch (error: unknown) {
+    res.send({
+      error,
+    });
+  }
+});
 
-app.listen(PORT, () => {
+const PORT = 3002;
++app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
